7295cfaec00efd3490170d7db63fff093c28cb1d
araujooj <gabrielrudy575@gmail.com>
2022-11-17 08:41:12 -0300

    app

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import { globalCss } from '@styles/global'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Checkbox } from '@ignite-ui/react'
+
+const Page = () => {
+  return (
+    <section>
+      <Checkbox />
+    </section>
+  )
+}
+
+export default Page
